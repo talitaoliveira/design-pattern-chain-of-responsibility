2a63eeaedeb614cb0ab08e2ef5b815145a0f216a
Talita Oliveira <litaa.oliveira@gmail.com>
2020-06-06 20:27:01 -0300

    Update description of course time

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ Criterios de aceitacao
 - Não ter a carteirinha da OAB
 - Ter feito as duas fases da prova
 - Nota acima 7 (sei la)
-- Estar a 2 períodos de terminar o curso
+- Estar a 2 períodos de terminar o curso (total: 10 periodos)
  */
 
 console.log("ola");
