1ffa6e427fc3c34c783d95d89201ad8306ad81c4
Talita Oliveira <litaa.oliveira@gmail.com>
2020-06-06 20:12:30 -0300

    Add test case failing to another scenario

--- src/validacoes.test.ts
+++ src/validacoes.test.ts
@@ -72,5 +72,22 @@ describe("Valicações", () => {
       // then
       expect(proximaValidacao).not.toHaveBeenCalled();
     });
+
+    it("não deve chamar a proxima validação caso a pessoa não tenha feito a primeira fase da prova", () => {
+      //given
+      const pessoa = {
+        carteiraOAB: false,
+        primeiraFase: false,
+        segundaFase: true,
+        notaPrimeiraFase: 7,
+        notaSegundaFase: 0,
+        periodo: 8,
+      };
+      const proximaValidacao = jest.fn();
+      // when
+      validaFezProvaDuasFases(pessoa, proximaValidacao);
+      // then
+      expect(proximaValidacao).not.toHaveBeenCalled();
+    });
   });
 });
