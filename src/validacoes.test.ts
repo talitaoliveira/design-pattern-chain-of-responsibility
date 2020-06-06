86666132476980608d7e149d2b6ae063c49afc03
Talita Oliveira <litaa.oliveira@gmail.com>
2020-06-06 19:26:30 -0300

    Add describe to group test of the specific function

--- src/validacoes.test.ts
+++ src/validacoes.test.ts
@@ -1,35 +1,36 @@
 import { validaPossuiACarteira } from "./validacoes";
 
 describe("Valicações", () => {
-  it("deve validar se a pessoa já possui a carteirinha ou nao", () => {
-    // given
-    const pessoa = {
-      carteiraOAB: false,
-      notaPrimeiraFase: 7,
-      notaSegundaFase: 8,
-      periodo: 8,
-    };
-    const proximaValidacao = jest.fn();
-    // when
-    const possuiCarteira = validaPossuiACarteira(pessoa, proximaValidacao);
-    // then
-    expect(possuiCarteira).toBe(false);
-  });
+  describe("Validação da carteirinha", () => {
+    it("deve validar se a pessoa já possui a carteirinha ou nao", () => {
+      // given
+      const pessoa = {
+        carteiraOAB: false,
+        notaPrimeiraFase: 7,
+        notaSegundaFase: 8,
+        periodo: 8,
+      };
+      const proximaValidacao = jest.fn();
+      // when
+      const possuiCarteira = validaPossuiACarteira(pessoa, proximaValidacao);
+      // then
+      expect(possuiCarteira).toBe(false);
+    });
+    it("deve chamar a proxima validacao se a pessoa não possui carteirinha", () => {
+      // given
+      const pessoa = {
+        carteiraOAB: false,
+        notaPrimeiraFase: 7,
+        notaSegundaFase: 8,
+        periodo: 8,
+      };
+      const proximaValidacao = jest.fn();
 
-  it("deve chamar a proxima validacao se a pessoa não possui carteirinha", () => {
-    // given
-    const pessoa = {
-      carteiraOAB: false,
-      notaPrimeiraFase: 7,
-      notaSegundaFase: 8,
-      periodo: 8,
-    };
-    const proximaValidacao = jest.fn();
+      // when
+      const possuiCarteira = validaPossuiACarteira(pessoa, proximaValidacao);
 
-    // when
-    const possuiCarteira = validaPossuiACarteira(pessoa, proximaValidacao);
-
-    // then
-    expect(proximaValidacao).toHaveBeenCalled();
+      // then
+      expect(proximaValidacao).toHaveBeenCalled();
+    });
   });
 });
