4ff84557aa3f8b56ecfa7ca67fb822bbb9537a9f
Talita Oliveira <litaa.oliveira@gmail.com>
2020-06-06 19:28:17 -0300

    Add coment with acceptance criterias

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
+/**
+Criterios de aceitacao
+- Não ter a carteirinha da OAB
+- Ter feito as duas fases da prova
+- Nota acima 7 (sei la)
+- Estar a 2 períodos de terminar o curso
+ */
+
 console.log("ola");
