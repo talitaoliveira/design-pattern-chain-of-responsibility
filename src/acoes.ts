ed404388aa3dedb413fe583c4407167ec15aba5c
Talita Oliveira <litaa.oliveira@gmail.com>
2020-06-07 15:42:07 -0300

    Show person's name on action

--- src/acoes.ts
+++ src/acoes.ts
@@ -1,6 +1,8 @@
 import { PessoaInscrita } from "./types";
 
 export const acaoEntregarCarteira = (pessoaInscrita: PessoaInscrita) => {
-  console.log("Parabéns, tome aqui sua carteirinha da OAB!");
+  console.log(
+    `Parabéns ${pessoaInscrita.nome}, tome aqui sua carteirinha da OAB!`
+  );
   return "Parabéns, tome aqui sua carteirinha da OAB!";
 };
