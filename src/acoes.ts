ce15d0bf2a15b67f5666547599e78ee2316266e4
Talita Oliveira <litaa.oliveira@gmail.com>
2020-06-07 13:21:11 -0300

    add log

--- src/acoes.ts
+++ src/acoes.ts
@@ -1,5 +1,6 @@
 import { PessoaInscrita } from "./types";
 
 export const acaoEntregarCarteira = (pessoaInscrita: PessoaInscrita) => {
+  console.log("Parabéns, tome aqui sua carteirinha da OAB!");
   return "Parabéns, tome aqui sua carteirinha da OAB!";
 };
